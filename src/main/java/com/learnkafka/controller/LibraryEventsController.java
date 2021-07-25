46ac736715e53e41ea2433c9744afa901c78704f
Abhilash V <abhilash.vannarath@navigantbpm.com>
2021-07-25 22:51:30 +0530

    Kafka in Synchronous way

--- src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -7,9 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @Slf4j
@@ -22,6 +25,16 @@ public class LibraryEventsController {
         // invoke kafka producer
         log.info("before sendLibraryEvent");
         libraryEventProducer.sendLibraryEvent(libraryEvent);
+        log.info("after sendLibraryEvent");
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PostMapping("/v1/libraryEventSynchronous")
+    public ResponseEntity<LibraryEvent> postLibraryEventSynchronous(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
+        // invoke kafka producer
+        log.info("before sendLibraryEvent");
+        SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
+        log.info("Send Result is {}", sendResult.toString());
         log.info("after sendLibraryEvent");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
