4ce050d7066f4c966075b7e7f99241f5740e07ae
Abhilash V <abhilash.vannarath@navigantbpm.com>
2021-07-26 00:05:42 +0530

    Added Test for libraryEvent

--- src/test/java/intg/com/learnkafka/controller/LibraryEventsControllerIntegrationTest.java
+++ src/test/java/intg/com/learnkafka/controller/LibraryEventsControllerIntegrationTest.java
@@ -1,9 +1,14 @@
 package com.learnkafka.controller;
 
+import com.learnkafka.domain.Book;
+import com.learnkafka.domain.LibraryEvent;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LibraryEventsControllerIntegrationTest {
@@ -12,6 +17,27 @@ public class LibraryEventsControllerIntegrationTest {
 
     @Test
     void  postLibraryEvent(){
+        // given
+        Book book = Book.builder()
+                .bookId(123)
+                .bookAuthor("Name")
+                .bookName("Kafka Spring Boot Testing")
+                .build();
 
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.valueOf(MediaType.APPLICATION_JSON.toString()));
+        headers.set("content-type", MediaType.APPLICATION_JSON.toString());
+        HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent, headers);
+
+        // when
+        ResponseEntity<LibraryEvent> responseEntity = restTemplate.exchange("/v1/postLibraryEvent", HttpMethod.POST, request, LibraryEvent.class);
+
+        // then
+        assertEquals(HttpStatus.CREATED, responseEntity);
     }
 }
