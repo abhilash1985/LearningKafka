d8e59d1be4f4ed96dcff9380e969854e225e3372
Abhilash V <abhilash.vannarath@navigantbpm.com>
2021-07-25 23:41:51 +0530

    Added Integration Test

---
+++ src/test/java/intg/com/learnkafka/controller/LibraryEventsControllerIntegrationTest.java
@@ -0,0 +1,17 @@
+package com.learnkafka.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LibraryEventsControllerIntegrationTest {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void  postLibraryEvent(){
+
+    }
+}
