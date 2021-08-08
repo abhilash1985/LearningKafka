9fd58e76594e5c06788d0e363c59cabf320ad958
Abhilash V <abhilash.vannarath@navigantbpm.com>
2021-08-09 00:27:21 +0530

    Unit Testing for Success

--- src/test/java/unit/com/learnkafka/producer/LibraryEventProducerUnitTest.java
+++ src/test/java/unit/com/learnkafka/producer/LibraryEventProducerUnitTest.java
@@ -6,6 +6,8 @@ import com.learnkafka.domain.Book;
 import com.learnkafka.domain.LibraryEvent;
 import kafka.Kafka;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,7 +15,12 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.SettableListenableFuture;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.isA;
@@ -31,7 +38,7 @@ public class LibraryEventProducerUnitTest {
     LibraryEventProducer eventProducer;
 
     @Test
-    void sendLibraryEventWithSend() throws JsonProcessingException {
+    void sendLibraryEventWithSendFailure() throws JsonProcessingException, ExecutionException, InterruptedException {
         // given
         Book book = Book.builder()
                 .bookId(123)
@@ -47,9 +54,41 @@ public class LibraryEventProducerUnitTest {
         SettableListenableFuture future = new SettableListenableFuture();
         future.setException(new RuntimeException("Exception Calling Kafka"));
 
+        // when
+        when(kafkaTemplate.send(isA(ProducerRecord.class))).thenReturn(future);
+
+        // then
+        assertThrows(Exception.class, ()-> eventProducer.sendLibraryEventWithSend(libraryEvent).get());
+    }
+
+    @Test
+    void sendLibraryEventWithSendSuccess() throws JsonProcessingException, ExecutionException, InterruptedException {
+        // given
+        Book book = Book.builder()
+                .bookId(123)
+                .bookAuthor("Name")
+                .bookName("Kafka Spring Boot Testing")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        SettableListenableFuture future = new SettableListenableFuture();
+
+        String record = objectMapper.writeValueAsString(libraryEvent);
+        ProducerRecord<Integer, String> producerRecord = new ProducerRecord("library-events", libraryEvent.getLibraryEventId(), record);
+        RecordMetadata recordMetadata = new RecordMetadata(new TopicPartition("library-events", 1), 1, 1, 342, System.currentTimeMillis(), 1, 2);
+        SendResult<Integer, String> sendResult = new SendResult<Integer, String>(producerRecord, recordMetadata);
+        future.set(sendResult);
+
         when(kafkaTemplate.send(isA(ProducerRecord.class))).thenReturn(future);
         // when
-        assertThrows(Exception.class, ()-> eventProducer.sendLibraryEventWithSend(libraryEvent).get());
+        ListenableFuture<SendResult<Integer, String>> listenableFuture = eventProducer.sendLibraryEventWithSend(libraryEvent);
+
         // then
+        SendResult<Integer, String> sendResult1 = listenableFuture.get();
+        assert sendResult1.getRecordMetadata().partition() == 1;
     }
 }
