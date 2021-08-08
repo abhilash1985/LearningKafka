735a44d609f515ce220d9b1d8df426a079c6a02e
Abhilash V <abhilash.vannarath@navigantbpm.com>
2021-08-08 21:22:21 +0530

    Unit Test for LibraryEventsController

---
+++ src/test/java/unit/com/learnkafka/controller/LibraryEventsControllerUnitTest.java
@@ -0,0 +1,7 @@
+package com.learnkafka.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(LibraryEventsController.class)
+public class LibraryEventsControllerUnitTest {
+}
