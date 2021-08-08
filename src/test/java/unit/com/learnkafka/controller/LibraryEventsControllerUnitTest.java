f9c6569fb59609b2facceb6e1f299b7152464e33
Abhilash V <abhilash.vannarath@navigantbpm.com>
2021-08-08 21:23:59 +0530

    Unit Test for LibraryEventsController with MockMVC

--- src/test/java/unit/com/learnkafka/controller/LibraryEventsControllerUnitTest.java
+++ src/test/java/unit/com/learnkafka/controller/LibraryEventsControllerUnitTest.java
@@ -1,7 +1,23 @@
 package com.learnkafka.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(LibraryEventsController.class)
+@AutoConfigureMockMvc
 public class LibraryEventsControllerUnitTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void postLibraryEvent() {
+        // given
+
+        // when
+
+        // then
+    }
 }
